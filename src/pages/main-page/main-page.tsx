f9fdbb38c3e820de6e0bb9677e83464f4d41e748
Byankina <141526746+53400755Maria@users.noreply.github.com>
2025-03-20 13:12:54 +0300

    Update main-page.tsx

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,6 @@
 import {JSX} from 'react';
 import CitiesCard from '../../components/cities-card/cities-card';
+import { Logo } from '../../components/logo/logo';
 
 type MainPageProps = {
     rentalOffersCount: number;
@@ -12,10 +13,7 @@ function MainPage({rentalOffersCount}: MainPageProps): JSX.Element {
             <div className ="container">
             <div className ="header__wrapper">
                 <div className ="header__left">
-                <a className ="header__logo-link header__logo-link--active">
-                    <img className ="header__logo" src="img/logo.svg" alt="Rent service logo" width="81" height="41">
-                    </img>
-                </a>
+                <Logo/>
                 </div>
                 <nav className ="header__nav">
                 <ul className ="header__nav-list">
@@ -97,47 +95,12 @@ function MainPage({rentalOffersCount}: MainPageProps): JSX.Element {
                     </ul>
                 </form>
                 <div className ="cities__places-list places__list tabs__content">
-                    <article className ="cities__card place-card">
-                    <div className ="place-card__mark">
-                        <span>Premium</span>
-                    </div>
-                    <div className ="cities__image-wrapper place-card__image-wrapper">
-                        <a href="#">
-                        <img className ="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image">
-                        </img>
-                        </a>
-                    </div>
-                    <div className ="place-card__info">
-                        <div className ="place-card__price-wrapper">
-                        <div className ="place-card__price">
-                            <b className ="place-card__price-value">&euro;120</b>
-                            <span className ="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className ="place-card__bookmark-button button" type="button">
-                            <svg className ="place-card__bookmark-icon" width="18" height="19">
-                            <use href="#icon-bookmark"></use>
-                            </svg>
-                            <span className ="visually-hidden">To bookmarks</span>
-                        </button>
-                        </div>
-                        <div className ="place-card__rating rating">
-                        <div className ="place-card__stars rating__stars">
-                            <span style={{width: "80%"}}></span>
-                            <span className ="visually-hidden">Rating</span>
-                        </div>
-                        </div>
-                        <h2 className ="place-card__name">
-                        <a href="#">Beautiful &amp; luxurious apartment at great location</a>
-                        </h2>
-                        <p className ="place-card__type">Apartment</p>
-                    </div>
-                    </article>
+                    <CitiesCard/>
+                    <CitiesCard/>
+                    <CitiesCard/>
+                    <CitiesCard/>
+                    <CitiesCard/>
                 </div>
-                <CitiesCard/>
-                <CitiesCard/>
-                <CitiesCard/>
-                <CitiesCard/>
-                <CitiesCard/>
                 <div className="cities__right-section">
               <section className="cities__map map"></section>
               </div>
