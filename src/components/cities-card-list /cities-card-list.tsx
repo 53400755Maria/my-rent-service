e15a72eff72bfb5b4b99b15043fc3eb6514a7c93
Byankina <141526746+53400755Maria@users.noreply.github.com>
2025-04-07 21:51:03 +0300

    Create cities-card-list.tsx

---
+++ src/components/cities-card-list /cities-card-list.tsx
@@ -0,0 +1,30 @@
+import { OffersList } from '../../types/offer';
+import CitiesCard from '../cities-card/cities-card';
+
+type CitiesCardListProps = {
+    offersList: OffersList[];
+    onListItemHover?: (offerId: string | null) => void;
+};
+
+function CitiesCardList({ offersList, onListItemHover }: CitiesCardListProps) {
+    return(
+        <div className="cities__places-list places__list tabs__content">
+            {offersList.map((item) => (
+                <CitiesCard 
+                    key={item.id}
+                    id={item.id}
+                    title={item.title}
+                    type={item.type}
+                    price={item.price}
+                    previewImage={item.previewImage}
+                    isPremium={item.isPremium}
+                    rating={item.rating}
+                    onMouseEnter={() => onListItemHover?.(item.id)}
+                    onMouseLeave={() => onListItemHover?.(null)}
+                />
+            ))}
+        </div>
+    );
+}
+
+export { CitiesCardList };
