ec090eac9df60c44479dffe08872339f80a23bc6
Byankina <141526746+53400755Maria@users.noreply.github.com>
2025-04-07 21:50:20 +0300

    Update app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,54 @@
 import MainPage from "../../pages/main-page/main-page";
+import Favorites from "../../pages/favorites/favorites";
+import Login from "../../pages/login/login";
+import Offer from "../../pages/offer/offer";
+import { PrivateRoute } from "../private-route/private-route";
+import Error from "../../pages/error-page/error";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { AppRoute } from "../../const";
+import { AuthorizationStatus } from "../../const";
 import {JSX} from 'react';
+import { FullOffer, OffersList } from "../../types/offer";
 
 type AppMainPageProps = {
     rentalOffersCount: number;
+    offersList: OffersList[];
+    offers: FullOffer[];
 }
 
-function App({rentalOffersCount}: AppMainPageProps): JSX.Element{
+function App({offers}: AppMainPageProps): JSX.Element{
     return (
-        <MainPage rentalOffersCount={rentalOffersCount}/>
+        <BrowserRouter>
+        <Routes>
+            <Route
+            path={AppRoute.Main}
+            element={<MainPage />}/>
+            
+            <Route
+            path={ AppRoute.Favorites }
+            element={
+              <PrivateRoute
+                authorizationStatus={ AuthorizationStatus.Auth}
+              >
+                <Favorites offers={offers}/>
 
+              </PrivateRoute>
+            }
+          />
+            
+            <Route
+            path={`${AppRoute.Offer}/:id`}
+            element={<Offer offers={offers}/>}/>
+            
+            <Route
+            path={AppRoute.Login}
+            element={<Login/>}/>
+            
+            <Route
+            path="*"
+            element={<Error/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 export default App;
